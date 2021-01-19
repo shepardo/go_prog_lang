94929cb5c93f9b7ac6c9d2abdd67db3671f0cbb6
Fernando Gonzalez Sanchez <fernando.gonzalez@wizeline.com>
2021-01-18 23:44:52 -0600

    Adding final version of popcount driver.

--- ch2/popcount_main.go
+++ ch2/popcount_main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"popcount"
 )
 
 func main() {
-  fmt.Printf("Hello World");
+	//fmt.Printf("Hello World\n");
+	for i := 0; i <= 64; i++ {
+		fmt.Printf("%d = %d\n", i, popcount.PopCount(uint64(i)));
+	}
 }
