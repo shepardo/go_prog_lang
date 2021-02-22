4b754f3ab55c963b125ea04e34a25d98a99bff75
Fernando Gonzalez Sanchez <fernando.gonzalez@wizeline.com>
2021-02-21 23:05:17 -0600

    Ch3, fixing a typo for surface sample.

--- ch3/surface.go
+++ ch3/surface.go
@@ -8,7 +8,7 @@ import (
 const (
 	width, height 		= 600, 320 // canvas size in pixels
 	cells 						= 100 // number of grid cells
-	xyrange 					= 30.0 // axis ranges (xyrange..+xyrange)
+	xyrange 					= 30.0 // axis ranges (-xyrange..+xyrange)
 	xyscale 					= width / 2 / xyrange // pixels per x or y unit
 	zscale 						= height * 0.4 // pixels per z unit
 	angle 						= math.Pi / 6 // angle of x, y axes (=30°)
@@ -44,7 +44,7 @@ func corner(i, j int) (float64, float64) {
 	sy := height / 2 + (x + y) * sin30 * xyscale - z * zscale
 	return sx, sy
 }
-	
+
 func f(x, y float64) float64 {
 	r := math.Hypot(x, y) // distance from (0,0)
 	return math.Sin(r) / r
