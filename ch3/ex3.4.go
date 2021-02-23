005bdd66e965614e3554722961f127abd9e648da
Fernando Gonzalez Sanchez <fernando.gonzalez@wizeline.com>
2021-02-22 23:40:30 -0600

    Ch3, first checkpoint for solution of Ex 3.4.

---
+++ ch3/ex3.4.go
@@ -0,0 +1,108 @@
+/*
+Exercise 3.4: Following the approach of the Lissajous example in Section 1.7, construct a web
+server that computes surfaces and writes SVG data to the client. The server must set the ContentType
+header like this:
+w.Header().Set("ContentType",
+"image/svg+xml")
+(This step was not required in the Lissajous example because the server uses standard
+heuristics to recognize common formats like PNG from the first 512 bytes of the response and
+generates the proper header.) Allow the client to specify values like height, width, and color as
+HTTP request parameters.
+*/
+
+// Surface computes an SVG rendering of a 3D surface function.
+package main
+import (
+	"fmt"
+	"math"
+	"os"
+	//"log"
+)
+
+const (
+	width, height 		= 600, 320 // canvas size in pixels
+	cells 						= 100 // number of grid cells
+	xyrange 					= 30.0 // axis ranges (-xyrange..+xyrange)
+	xyscale 					= width / 2 / xyrange // pixels per x or y unit
+	zscale 						= height * 0.4 // pixels per z unit
+	angle 						= math.Pi / 6 // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' " +
+		"style='stroke: grey; fill: white; strokewidth:0.7' " +
+		"width='%d' height='%d'>", width, height)
+	//l := log.New(os.Stderr, "", 0)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, z := corner(i+1, j)
+			color := "#ffffff"
+			if z >= 0.0 {
+				color = "#ff0000"
+			} else {
+				color = "#0000ff"
+			}
+			if math.IsNaN(ay) {
+				fmt.Fprintf(os.Stderr, "ay is NaN for %d, %d\n", i + 1, j)
+				continue
+			}
+			bx, by, z := corner(i, j)
+			if math.IsNaN(by) {
+				fmt.Fprintf(os.Stderr, "by is NaN for %d, %d\n", i, j)
+				continue
+			}
+			cx, cy, z := corner(i, j+1)
+			if math.IsNaN(cy) {
+				fmt.Fprintf(os.Stderr, "cy is NaN for %d, %d\n", i, j+1)
+				continue
+			}
+			dx, dy, z := corner(i+1, j+1)
+			if math.IsNaN(dy) {
+				fmt.Fprintf(os.Stderr, "dy is NaN for %d, %d\n", i+1, j+1)
+				continue
+			}
+			fmt.Printf("<polygon fill='%s' points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				color, ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Println("</svg>")
+}
+
+func corner(i, j int) (float64, float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i) / cells - 0.5)
+	y := xyrange * (float64(j) / cells - 0.5)
+	// Compute surface height z.
+	z := f(x, y)
+	// Project (x,y,z) isometrically onto 2D SVG canvas (sx,sy).
+	sx := width / 2 + (x - y) * cos30 * xyscale
+	sy := height / 2 + (x + y) * sin30 * xyscale - z * zscale
+	return sx, sy, z
+}
+
+func eggbox(x, y float64) float64 {
+	return 0.2 * (math.Cos(x) + math.Cos(y))
+}
+
+func saddle(x, y float64) float64 {
+	a := 25.0
+	b := 17.0
+	a2 := a * a
+	b2 := b * b
+	return (y*y/a2 - x*x/b2)
+}
+
+func mogul(x, y float64) float64 {
+	return math.Abs(math.Sin(x) * math.Sin(y))
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
+	//return math.Sin(x) * math.Sin(y)
+	//return eggbox(x, y)
+	//return saddle(x, y)
+	//return mogul(x, y)
+}
